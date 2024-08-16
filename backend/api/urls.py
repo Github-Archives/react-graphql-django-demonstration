1ed0351d95434095a41e20102bdbb6a8afe524db
Github-Archives <drewreynerwilliams@gmail.com>
2024-08-15 22:25:56 -0400

    add commented next option for graphene

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+# * Graphene GraphQL attempt
+# from graphene_django.views import GraphQLView
+# from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', views.home, name='home'),  # Define a view for the root URL
+    # * Graphene GraphQL attempt
+    # path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
