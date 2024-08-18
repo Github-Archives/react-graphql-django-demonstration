a31876634de90c8d2233e05a3355a51913f318e2
Github-Archives <drewreynerwilliams@gmail.com>
2024-08-17 23:20:38 -0400

    add schema test function and add settings_example.py for users

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -11,9 +11,13 @@ class BookType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_books = graphene.List(BookType)
+    book_by_id = graphene.Field(BookType, id=graphene.Int())
 
     def resolve_all_books(self, info):
         return Book.objects.all()
+
+    def resolve_book_by_id(self, info, id):
+        return Book.objects.get(pk=id)
 
 
 schema = graphene.Schema(query=Query)
