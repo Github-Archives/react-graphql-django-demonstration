58908a825f8cd4efdc10dc97a5929c5b73ac86d6
Github-Archives <drewreynerwilliams@gmail.com>
2024-08-23 17:10:01 -0400

    init commit

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -1,14 +1,21 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-from .models import Book
+from .models import Book, MyModel
 
 
-# * Test Query
+# Define BookType
 class BookType(DjangoObjectType):
     class Meta:
         model = Book
 
 
+# Define MyModelType
+class MyModelType(DjangoObjectType):
+    class Meta:
+        model = MyModel
+
+
+# Define Queries
 class Query(graphene.ObjectType):
     all_books = graphene.List(BookType)
     book_by_id = graphene.Field(BookType, id=graphene.Int())
@@ -20,4 +27,25 @@ class Query(graphene.ObjectType):
         return Book.objects.get(pk=id)
 
 
-schema = graphene.Schema(query=Query)
+# Define CreateMyModel Mutation
+class CreateMyModel(graphene.Mutation):
+    class Arguments:
+        field1 = graphene.String(required=True)
+        field2 = graphene.String(required=True)
+        field3 = graphene.String(required=True)
+
+    mymodel = graphene.Field(MyModelType)
+
+    def mutate(self, info, field1, field2, field3):
+        mymodel = MyModel(field1=field1, field2=field2, field3=field3)
+        mymodel.save()
+        return CreateMyModel(mymodel=mymodel)
+
+
+# Define Mutations
+class Mutation(graphene.ObjectType):
+    create_mymodel = CreateMyModel.Field()
+
+
+# Define Schema
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useQuery } from "@apollo/client"
 import { GET_BOOKS } from "./queries"
 import FormComponent from "./components/FormComponent"
@@ -6,29 +7,34 @@ import "./App.css"
 
 function App() {
   // * From api_book db table
-  const { loading, error, data } = useQuery(GET_BOOKS)
+  const { loading, error, data, refetch } = useQuery(GET_BOOKS)
+  // * WebSocket/Daphne
   // * From api_mymodel db table
-  const [realTimeData, setRealTimeData] = useState([])
+  // const [realTimeData, setRealTimeData] = useState([])
 
+  // useEffect(() => {
+  //   // WebSocket Connection: The useEffect hook establishes a WebSocket connection when the component mounts and closes it when the component unmounts.
+  //   const socket = new WebSocket(
+  //     "ws://" + window.location.hostname + ":8000/ws/api_mymodel/"
+  //   )
+  //   socket.onmessage = function (e) {
+  //     const newData = JSON.parse(e.data)
+  //     setRealTimeData((prevData) => [...prevData, newData])
+  //   }
+  //   socket.onclose = function (e) {
+  //     console.error("WebSocket closed unexpectedly")
+  //   }
+  //   return () => {
+  //     socket.close()
+  //   }
+  // }, [])
+
+  // * Refetch Data on Data Change:
+  // Added a useEffect hook that calls refetch() whenever data changes to ensure the latest data is fetched after a mutation.
+  // This ensures that your component will refetch the data from the GraphQL server whenever new data is added, without relying on WebSockets for real-time updates.
   useEffect(() => {
-    // WebSocket Connection: The useEffect hook establishes a WebSocket connection when the component mounts and closes it when the component unmounts.
-    const socket = new WebSocket(
-      "ws://" + window.location.hostname + ":8000/ws/api_mymodel/"
-    )
-
-    socket.onmessage = function (e) {
-      const newData = JSON.parse(e.data)
-      setRealTimeData((prevData) => [...prevData, newData])
-    }
-
-    socket.onclose = function (e) {
-      console.error("WebSocket closed unexpectedly")
-    }
-
-    return () => {
-      socket.close()
-    }
-  }, [])
+    refetch()
+  }, [data])
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
@@ -47,7 +53,7 @@ function App() {
           </li>
         ))}
       </ul>
-      <h2>Daphne Websocket Server for real-time updates (not using GraphQL)</h2>
+      {/* <h2>Daphne Websocket Server for real-time updates (not using GraphQL)</h2>
       <ul>
         {realTimeData.map((item, index) => (
           <li key={index}>
@@ -57,7 +63,7 @@ function App() {
             <p>Field3: {item.field3}</p>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   )
 }
