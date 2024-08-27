7c48b25d8a50400476fff9d0c78ddf214dbd20e0
Github-Archives <drewreynerwilliams@gmail.com>
2024-08-26 20:45:12 -0400

    add bloodtype models and initial schema3

--- backend/api/models.py
+++ backend/api/models.py
@@ -19,10 +19,9 @@ class BloodTypes(models.Model):
     def __str__(self):
         return self.county_name
 
-
-#     description = models.TextField()
-
 # * Test Book Model
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
